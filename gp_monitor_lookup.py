33b4bab4901a3da3d207447f9a2c4e816b6dbefb
Dr Natasha Hurley-Walker <nhw@icrar.org>
2022-05-17 16:40:44 +0800

    Added option to search +/-12h around a given calibrator

--- gp_monitor_lookup.py
+++ gp_monitor_lookup.py
@@ -8,12 +8,12 @@ import numpy as np
 
 import datetime
 
-from astropy.io import fits
+#from astropy.io import fits
 from astropy.time import Time
 from astropy import units as u
-from astropy.coordinates import SkyCoord
+#from astropy.coordinates import SkyCoord
 
-from glob import glob
+#from glob import glob
 
 import urllib.request
 import json
@@ -114,30 +114,31 @@ def do_lookup(
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    group1 = parser.add_argument_group("Input/output files")
-#    group1.add_argument("--ra", dest='ra', type=float, default=246.99796,
+#    parser.add_argument("--ra", dest='ra', type=float, default=246.99796,
 #                        help="RA / longitude value (deg)")
-#    group1.add_argument("--dec", dest='dec', type=float, default=-52.58453,
+#    parser.add_argument("--dec", dest='dec', type=float, default=-52.58453,
 #                        help="Dec / latitude value (deg)")
-    group1.add_argument("--startdate", dest='startdate', type=str, default=None,
+    parser.add_argument("--startdate", dest='startdate', type=str, default=None,
                         help="start UTC date-time for search (format: 'YYYY-MM-DD HH:MM:SS' ; default = now-24h)")
-    group1.add_argument("--stopdate", dest='stopdate', type=str, default=None,
+    parser.add_argument("--stopdate", dest='stopdate', type=str, default=None,
                         help="stop UTC date-time for search (format: 'YYYY-MM-DD HH:MM:SS' ; default = now)")
-    group1.add_argument("--project", dest='project', type=str, default='G0080',
+    parser.add_argument("--project", dest='project', type=str, default='G0080',
                         help="MWA observing project (default = G0080)")
-    group1.add_argument("--preferred calibrator", dest='calsrc', type=str, default='HerA',
+    parser.add_argument("--preferred calibrator", dest='calsrc', type=str, default='HerA',
                         help="Preferred calibrator (default = HerA; options=HerA, J063633-204225, J121834-101851, J153150+240244")
-#    group1.add_argument("--minchan", dest='minchan', type=int, default=60,
+#    parser.add_argument("--minchan", dest='minchan', type=int, default=60,
 #                        help="minimum coarse channel number (default = 60)")
-#    group1.add_argument("--maxchan", dest='maxchan', type=int, default=180,
+#    parser.add_argument("--maxchan", dest='maxchan', type=int, default=180,
 #                        help="maximum coarse channel for search (default = 180)")
-    group1.add_argument("--cal", dest='cal', action="store_true", default=False,
+    parser.add_argument("--cal", dest='cal', action="store_true", default=False,
                         help="only look for calibrator observations (will only return most recent)")
-#    group1.add_argument("--separation", dest='separation', type=float, default=50.,
+    parser.add_argument("--calid", dest='calid', type=int, default=None,
+                        help="Look for data surrounding a particular calibrator (overrides other search options)"
+#    parser.add_argument("--separation", dest='separation', type=float, default=50.,
 #                        help="Maximum allowable sky separation between pointing centre and source (default = 50deg)")
-    group1.add_argument("--output", dest='output', type=str, default=None,
+    parser.add_argument("--output", dest='output', type=str, default=None,
                         help="Output text file for search args (default = '<project>_<startdate>-<stopdate>_search.txt' (colons will be stripped)")
-    group1.add_argument(
+    parser.add_argument(
         '--skip-db-check', 
         default=False,
         action='store_true',
@@ -156,16 +157,22 @@ if __name__ == "__main__":
     if args.verbose:
         logger.setLevel(logging.DEBUG)
 
-    if args.startdate is None:
-        duration = datetime.timedelta(hours = 24)
-        start = datetime.datetime.utcnow() - duration
-    else:
-        start = datetime.datetime.strptime(args.startdate,"%Y-%m-%d %H:%M:%S")
+# TODO: replace all datetime calls with astropy Time calls
+    if args.calid is None:
+        if args.startdate is None:
+            duration = datetime.timedelta(hours = 24)
+            start = datetime.datetime.utcnow() - duration
+        else:
+            start = datetime.datetime.strptime(args.startdate,"%Y-%m-%d %H:%M:%S")
 
-    if args.stopdate is None:
-        stop = datetime.datetime.utcnow()
+        if args.stopdate is None:
+            stop = datetime.datetime.utcnow()
+        else:
+            stop = datetime.datetime.strptime(args.stopdate,"%Y-%m-%d %H:%M:%S")
     else:
-        stop = datetime.datetime.strptime(args.stopdate,"%Y-%m-%d %H:%M:%S")
+        gpstime = Time(args.calid, format="gps")
+        start = gpstime - 12*u.hour
+        stop = gpstime + 12*u.hour
 
     if args.cal is True:
         cal = 1
