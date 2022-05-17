908b08467dd14c9e3b831c687dc3eba3360f9a91
Natasha Hurley-Walker <nhw@icrar.org>
2022-05-17 15:23:47 +0800

    Made it a function so you can import it if necessary

--- determine_epoch.py
+++ determine_epoch.py
@@ -5,8 +5,12 @@
 import datetime
 
 # Epoch numberering starts 1st May 2022
-epoch0 = datetime.datetime.strptime("2022-05-01 00:00:00","%Y-%m-%d %H:%M:%S")
-now = datetime.datetime.utcnow()
-diff = now - epoch0
+def epoch():
+    epoch0 = datetime.datetime.strptime("2022-05-01 00:00:00","%Y-%m-%d %H:%M:%S")
+    now = datetime.datetime.utcnow()
+    diff = now - epoch0
 # Y2k is 2032
-print(f"{int(diff.days/7):03d}")
+    return(f"{int(diff.days/7):03d}")
+
+if __name__ == "__main__":
+    print(epoch())
