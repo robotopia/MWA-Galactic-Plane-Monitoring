3b382000441ef46444f29ad5e7c5b2d0c4ae52a6
Dr Natasha Hurley-Walker <nhw@icrar.org>
2022-05-17 16:28:46 +0800

    Added more expressive to-do list

--- gp_monitor.sh
+++ gp_monitor.sh
@@ -4,19 +4,29 @@
 #SBATCH --time=00:05:00
 #SBATCH --nodes=1
 
-# The regular monitoring script that runs every hour to see if a new calibrator has been observed
+# TODO: set up some environment variables so that the errors and outputs go to a sensible directory
+# Otherwise they will go to /home/$USER 
 
-epoch=$(determine_epoch.py)
-# TODO: make a directory based on the epoch and go there to do everything
-# TODO: Somehow make the errors and outputs of this script go to that directory!!
+# The regular monitoring script that runs every hour to see if a new calibrator has been observed
 
 obsid=$(gp_monitor_lookup.py --cal)
 
 if [[ $obsid != ""]]
 then
+    epoch=$(determine_epoch.py --obsid $obsid)
+# Note: output format is: Epoch%03d
+# TODO: first part of directory stem should probably be set from template or environment variables
+    pdir=/astro/mwasci/$USER/$epoch
+    if [[ ! -d $pdir ]]
+    then
+        mkdir $pdir
+    fi
     dep=($(obs_manta.sh -p $epoch -o $obsid))
     depend=${dep[3]}
     dep=($(obs_autoflag.sh -d ${depend} -p ${epoch} $obsnum))
     depend=${dep[3]}
     dep=($(obs_autocal.sh -d ${depend} -p ${epoch} $obsnum))
+    depend=${dep[3]}
+# TODO: template obs_gp_image.sh based on existing skeleton gp_image.sh
+# TODO: run obs_gp_image.sh with the new created dependency, -c $obsid -d $depend
 fi
