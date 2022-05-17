426da2484d1b11954020a8b65fc32d5b6bf687ac
Natasha Hurley-Walker <nhw@icrar.org>
2022-05-17 15:45:49 +0800

    First pass imaging script to run if the calibration works

---
+++ gp_image.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -l
+#SBATCH --account=mwasci
+#SBATCH --partition=workq
+#SBATCH --time=00:05:00
+#SBATCH --nodes=1
+
+# The gp_imaging script that, if the calibration is successful, runs on the rest of the data
+# It retries every hour to do any observation that hasn't yet been entered into our processing database
+# When every observation has been processed it no longer submits itself to the queue
+
+epoch=$(determine_epoch.py --obsid $CALID)
+epoch="Epoch"$epoch
+# TODO: define with environment variables
+pdir=/astro/mwasci/$USER/epoch
+cd $pdir
+
+# This does somewhat rely on the calibrator downloading and processing correctly within 24h
+# TODO: change this code so that you can give it a --pincal option
+obsid=$(gp_monitor_lookup.py --pincal $CALID)
+
+if [[ $obsid != ""]]
+then
+    for obs in $obsid
+    do
+        dep=($(obs_manta.sh -p $epoch -o $obsid))
+        depend=${dep[3]}
+        dep=($(obs_autoflag.sh -d ${depend} -p ${epoch} $obsnum))
+        depend=${dep[3]}
+        dep=($(obs_apply_cal.sh -d ${depend} -p "${epoch}" -c $CALID -z  $obsnum))
+        depend=${dep[3]}
+        dep=($(obs_uvflag.sh -d ${depend} -p "${epoch}" -z $obsnum))
+        depend=${dep[3]}
+        dep=($(obs_image.sh -d ${depend} -p "${epoch}" -z $obsnum))
+        depend=${dep[3]}
+        dep=($(obs_transient.sh -d ${depend} -p "${epoch}" $obsnum))
+    done
+fi
