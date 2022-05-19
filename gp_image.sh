e5b8df2a8f330d229779f00a11556e6be6046a6c
tim <tim.galvin@curtin.edu.au>
2022-05-19 17:00:19 +0800

    first pass plugging things together

--- gp_image.sh
+++ gp_image.sh
@@ -6,25 +6,41 @@
 
 # TODO: How do I easily make sure every python script runs in the singularity container
 # without prefixing everything with singularity exec ....
+# TJG: We could try a sneaky ./${GPMBASE}/script_name.py trick. 
+#      Not sure how this will work with other imported python scripts from this repo
+#      Might need to install in user space and map to it with pip install. Will play. 
 
 # The gp_imaging script that, if the calibration is successful, runs on the rest of the data
 # It retries every hour to do any observation that hasn't yet been entered into our processing database
 # When every observation has been processed it no longer submits itself to the queue
 
-epoch=$(determine_epoch.py --obsid $CALID)
-epoch="Epoch"$epoch
+EPOCH=$(determine_epoch.py --obsid $CALID)
+EPOCH="Epoch${EPOCH}"
+
 # TODO: define with environment variables
-datadir=${GPMSCRATCH}/$epoch
-cd $datadir
+DATADIR="${GPMSCRATCH}/${EPOCH}"
+cd "${DATADIR}" || exit 1
 
-# TODO: Insert test of calibration quality here. Only if the calibrator is good enough, proceed to the next step
+# Test to see if the calibration solution meets minimum quality control. At the moment
+# this is a simple check based on the number of flagged solutions
+SOLUTIONS="${CALID}/${CALID}solutions_initial_ref.bin"
 
+# Need to confirm the python script works
+result=$(check_solutions.py -t 0.25 check "${SOLUTIONS}")
+if echo "${result}" | grep -q fail
+then
+    mv "${SOLUTIONS}" "${SOLUTIONS%.bin}_ref_failed.bin"
+    echo "Calibration solutions file failed checks. Exiting. "
 
+    # Email goes here
+
+    exit 111
+fi
 
 # Find all the observations taken within +/-12h of that calibrator
 obsid=$(gp_monitor_lookup.py --calid $CALID)
 
-if [[ $obsid != ""]]
+if [[ $obsid != "" ]]
 then
     for obs in $obsid
     do
@@ -41,5 +57,3 @@ then
         dep=($(obs_transient.sh -d ${depend} -p "${epoch}" -z $obsnum))
     done
 fi
-
-# TODO the else statement for the calibration quality -- email the user to say that their calibration didn't work
