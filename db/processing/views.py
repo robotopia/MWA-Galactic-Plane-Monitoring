a40a0b4b5ca358adb2a59b4a2e3731e54959f0c3
Sammy McSweeney <sammy.mcsweeney@gmail.com>
2024-06-06 09:51:10 +0800

    Added submission times to epoch overview display

--- db/processing/views.py
+++ db/processing/views.py
@@ -6,7 +6,7 @@ def EpochOverviewView(request, epoch, user):
 
     observations = models.Observation.objects.all()
     observations = [o for o in observations if o.epoch == epoch]
-    overviews = {o: {eo.task: {"status": eo.status, "cal_obs": eo.cal_obs} for eo in o.epoch_overviews.all() if eo.user == user and eo.epoch == epoch} for o in observations}
+    overviews = {o: {eo.task: {"status": eo.status, "cal_obs": eo.cal_obs, "date": eo.submission_time} for eo in o.epoch_overviews.all() if eo.user == user and eo.epoch == epoch} for o in observations}
 
     context = {
         'epoch': epoch,
