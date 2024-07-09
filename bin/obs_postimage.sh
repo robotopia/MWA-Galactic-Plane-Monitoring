7daae172183dd886ec0e26c0b332b2ad769d756a
Sam McSweeney <sammy.mcsweeney@gmail.com>
2024-07-09 12:06:38 +0800

    Made the database 'task' for postimage include the pol as well (e.g. postimageI, postimageV)

--- bin/obs_postimage.sh
+++ bin/obs_postimage.sh
@@ -115,7 +115,7 @@ jobid=${jobid[3]}
 echo "Submitted ${script} as ${jobid} . Follow progress here:"
 
 # Add rows to the database 'processing' table that will track the progress of this submission
-${GPMCONTAINER} ${GPMBASE}/gpm_track.py create_jobs --jobid="${jobid}" --task='postimage' --batch_file="${script}" --obs_file="${obsnum}" --stderr="${error}" --stdout="${output}"
+${GPMCONTAINER} ${GPMBASE}/gpm_track.py create_jobs --jobid="${jobid}" --task="postimage${pol}" --batch_file="${script}" --obs_file="${obsnum}" --stderr="${error}" --stdout="${output}"
 ${GPMCONTAINER} ${GPMBASE}/gpm_track.py queue_jobs --jobid="${jobid}" --submission_time="$(date +%s)"
 
 echo "STDOUTs: ${output}"
