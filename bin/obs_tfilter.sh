cd713029b7a8b9d4d1d3e1d7e0ff15df1f0cc7f9
Sam McSweeney <sammy.mcsweeney@gmail.com>
2024-09-03 16:49:17 +0800

    Adjusted tfilter template and 'bin' file according to the new tfilter code. Untested, WIP. Still in 'draft' mode

--- bin/obs_tfilter.sh
+++ bin/obs_tfilter.sh
@@ -72,7 +72,7 @@ chmod 755 "${script}"
 echo '#!/bin/bash' > "${script}.sbatch"
 echo "singularity run ${GPMCONTAINER} ${script}" >> "${script}.sbatch"
 
-sub="sbatch --begin=now+5minutes --export=ALL  --time=12:00:00 --mem=${GPMABSMEMORY}G -M ${GPMCOMPUTER} --output=${output} --error=${error}"
+sub="sbatch --begin=now+5minutes --export=ALL  --time=01:00:00 --mem=10G -M ${GPMCOMPUTER} --output=${output} --error=${error}"
 sub="${sub} ${GPMNCPULINE} ${account} ${GPMTASKLINE} ${depend} ${queue} ${script}.sbatch"
 if [[ ! -z ${tst} ]]
 then
