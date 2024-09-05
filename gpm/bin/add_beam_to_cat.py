0f65438bf3ac3cda57a2fc5e0e8c79309c67bee0
Sam McSweeney <sammy.mcsweeney@gmail.com>
2024-09-05 14:03:41 +0800

    Further fix for add_beam_to_cat.py

--- gpm/bin/add_beam_to_cat.py
+++ gpm/bin/add_beam_to_cat.py
@@ -24,9 +24,14 @@ singlecoord = SkyCoord(tab["ra"][0], tab["dec"][0], unit=(u.deg, u.deg), frame="
 x, y = w.world_to_pixel(singlecoord)
 coordlist = SkyCoord(tab["ra"], tab["dec"], unit=(u.deg, u.deg), frame="fk5")
 x, y = w.world_to_pixel(coordlist)
-x -= 1
-y -= 1
+
+_, _, Ny, Nx = beamdata.shape
 x, y = x.astype(int), y.astype(int)
+x[x < 0] = 0
+y[y < 0] = 0
+x[x >= Nx] = Nx - 1
+y[y >= Ny] = Ny - 1
+
 tab["beam"] = np.squeeze(beamdata[:,:,y, x])
 
 tab.write(comp.replace(".fits", "_wbeam.fits"), overwrite=True)
