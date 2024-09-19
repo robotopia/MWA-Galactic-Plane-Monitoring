7c3aa10531f7e2419f25c003e11b2bc64cdf72d0
Sam McSweeney <sammy.mcsweeney@gmail.com>
2024-09-19 12:38:21 +0800

    WIP adding db source to image_value_at_radec

--- gpm/bin/image_value_at_radec.py
+++ gpm/bin/image_value_at_radec.py
@@ -1,11 +1,43 @@
 #!/usr/bin/env python
 
+import os
 import numpy as np
 from astropy.io import fits
 from astropy.wcs import WCS
 from astropy.coordinates import SkyCoord
 import astropy.units as u
 import argparse
+import logging
+import mysql.connector as mysql
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(format="%(module)s:%(lineno)d:%(levelname)s %(message)s")
+logger.setLevel(logging.INFO)
+
+def gpmdb_config():
+    host = os.environ["GPMDBHOST"]
+    port = os.environ["GPMDBPORT"]
+    user = os.environ["GPMDBUSER"]
+    passwd = os.environ["GPMDBPASS"]
+    database = "gp_monitor"
+
+    return {
+        "host": host,
+        "port": port,
+        "user": user,
+        "password": passwd,
+        "database": database,
+    }
+
+
+def gpmdb_connect():
+    db_config = gpmdb_config()
+
+    logger.debug(f"Connecting to GP Monitor database - {db_config['host']}:{db_config['port']}")
+    db_con = mysql.connect(**db_config)
+
+    return db_con
+
 
 def sc(data):
     std = np.std(data)
@@ -45,19 +77,32 @@ def main(fits_image, coords, err_coords=None, err_size=None):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Pull out the value of an image at the specified coordinates")
     parser.add_argument("fits_image", help="The path to the FITS image to open")
-    parser.add_argument("coords", help="The coordinates in 'HH:MM:SS.S_DD:MM:SS.S' format")
+    parser.add_argument("source", help="The source name (which must exist in the database) to use for coordinates")
     parser.add_argument("--error_ctr_coords", help="The coords (in 'HH:MM:SS.S_DD:MM:SS.S' format) for the centre of a region used to calculate the rms error. Default: same as COORDS")
     parser.add_argument("--error_region_size", default=200, type=int, help="The length of one side of a box (in pixels) used for the region used to calculate the rms error. Default: 200")
     args = parser.parse_args()
 
-    # Parse the sky coords
-    ra, dec = args.coords.split("_")
-    coords = SkyCoord(ra, dec, unit=(u.hour, u.deg), frame="fk5")
+    # Connect to the database
+    conn = gpmdb_connect()
+    cur = conn.cursor()
+
+    # Get the source coordinates for the named source from the database
+    cur.execute("SELECT raj2000, decj2000 FROM source WHERE name = %s", (args.source,))
+
+    res = cur.fetchone()
+    if len(res) == 0:
+        raise Exception(f"No source with was found with the name \"{args.source}\"")
+    ra, dec = res[0]
+
+    coords = SkyCoord(ra, dec, unit=(u.deg, u.deg), frame="fk5")
 
     # Parse the sky coords for the error region
-    err_ctr_coords_str = args.error_ctr_coords or args.coords
+    err_ctr_coords_str = args.error_ctr_coords or coords
     err_ra, err_dec = err_ctr_coords_str.split("_")
     err_coords = SkyCoord(err_ra, err_dec, unit=(u.hour, u.deg), frame="fk5")
 
     # Start main
     main(args.fits_image, coords, err_coords=err_coords, err_size=args.error_region_size)
+
+    # Disconnect from the database
+    conn.close()
