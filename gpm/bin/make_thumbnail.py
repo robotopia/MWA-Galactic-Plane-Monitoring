32de2b9a501882e4f104e2453def1dd2cae57609
Sam McSweeney <sammy.mcsweeney@gmail.com>
2024-09-19 12:20:37 +0800

    Changed aspect ratio for thumbnail images

--- gpm/bin/make_thumbnail.py
+++ gpm/bin/make_thumbnail.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 mpl.use("Agg")  # So does not use display
 
-def main(fits_image, coords, size):
+def main(fits_image, coords, size, outplot):
     # Open the FITS file
     hdul = fits.open(fits_image)
     w = WCS(hdul[0].header, naxis=2)
@@ -29,19 +29,18 @@ def main(fits_image, coords, size):
     wx0, wy0 = w.wcs_pix2world(x0, y0, 1)
     wx1, wy1 = w.wcs_pix2world(x1, y1, 1)
     extent = (wx0/15 + 24, wx1/15 + 24, wy0, wy1)
+    aspect = 1/(np.cos(coords.dec)*15)
         
     thumbnail = np.squeeze(hdul[0].data[:, :, y0:y1, x0:x1])
 
     fig = plt.figure(figsize=(4,3.5))
     fig.add_subplot(111)
-    plt.imshow(thumbnail, origin='lower', aspect='auto', cmap=plt.cm.viridis, extent=extent)
+    plt.imshow(thumbnail, origin='lower', aspect=aspect, cmap=plt.cm.viridis, extent=extent)
     plt.xlabel('RA (hours)')
     plt.ylabel('Dec (deg)')
 
-    #plt.xlim((int(x - size/2), int(x + size/2)))
-    #plt.ylim((int(y - size/2), int(y + size/2)))
     plt.tight_layout()
-    plt.savefig("foo.png")
+    plt.savefig(outplot)
 
     #print(hdul[0].header['DATE-OBS'], value, rms)
     #except:
@@ -55,11 +54,15 @@ if __name__ == "__main__":
     parser.add_argument("fits_image", help="The path to the FITS image to open")
     parser.add_argument("coords", help="The coordinates in 'HH:MM:SS.S_DD:MM:SS.S' format")
     parser.add_argument("--size", default=200, type=int, help="The number of pixels along one side of the (square) thumbnail. Default: 200")
+    parser.add_argument("--outplot", help="The output plot filename. Default: same as fits_image, but with \".fits\" replaced with \"_[COORDS].png\"")
     args = parser.parse_args()
 
     # Parse the sky coords
     ra, dec = args.coords.split("_")
     coords = SkyCoord(ra, dec, unit=(u.hour, u.deg), frame="fk5")
 
+    # Set output filename
+    outplot = args.outplot or args.fits_image.replace(".fits", f"_{ra}_{dec}.png")
+
     # Start main
-    main(args.fits_image, coords, args.size)
+    main(args.fits_image, coords, args.size, outplot)
